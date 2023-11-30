cab6eb72a4c746dfa405a914e1282853f3b5637d
山姆高德 <513107329@qq.com>
2023-12-01 00:17:00 +0800

    feat: project init

---
+++ main/index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800, 
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
+
+	win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+	createWindow()
+})
+
+app.on('window-all-closed', () => {
+	console.log('window-all-closed')
+	if(process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+	if(BrowserWindow.getAllWindows().length === 0) createWindow()
+})
